87d498ade4081ddcbeaa5d0b4f0906e7f9763f8b
Jerry Yi <jerry.yi@hirede.com>
2017-04-11 15:04:49 +0800

    饿了么移动端项目结构设计

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+export default {
+  name: 'app',
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="header">
+		我是header
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	.header
+		display: flex
+		text-align: right
+</style>
